f1953e626ef2418f1e2b3abd313627ab4af3cb3e
Travis Redington <trredington@gmail.com>
2026-08-28 14:39:47 -0400

    some basic content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,13 @@ import MainContent from "../components/ui/main-content/main-content";
 function HomePage() {
     return (
         <MainContent>
-            <p>This is the home page</p>
+            <h2>Hello!</h2>
+
+            <p>This is a Scratchpad app for <a href="https://www.redtrowl.com" target="_blank" rel="noopener noreferrer">Redtrowl</a></p>
+
+            <p>I figured I'd put random ideas for tech here. Why not?</p>
+
+            <p>This site is built by me and Claude Code as an assistant.</p>
         </MainContent>
     )
 }
